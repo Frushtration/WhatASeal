bc8f36ef822853e70fcf2815fa4e711b30d0f10b
Frush <cjfrush@ucdavis.edu>
2019-02-20 09:50:59 -0800

    Working audio with test playback

--- SealSaver4000/ViewController.swift
+++ SealSaver4000/ViewController.swift
@@ -5,14 +5,60 @@
 //  Created by Christie  on 1/19/19.
 //  Copyright © 2019 Christie Frush. All rights reserved.
 //
-
 import UIKit
+import AVFoundation
+import CoreAudio
+import CoreAudioKit
+import Foundation
+import AVKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, AVAudioRecorderDelegate , AVAudioPlayerDelegate {
+    
+    @IBOutlet weak var btnAudioRecord: UIButton!
+    var recordingSession : AVAudioSession!
+    var audioRecorder    :AVAudioRecorder!
+    var settings         = [String : Int]()
+    var audioPlayer : AVAudioPlayer!
 
+    
+    var startTime = TimeInterval()
+    
+    var timer1:Timer = Timer()
+    
+    @IBOutlet weak var frequency: UILabel!
+    
+    @IBOutlet weak var timer1Label: UILabel!
+    
+    @IBOutlet weak var timer2Label: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        recordingSession = AVAudioSession.sharedInstance()
+        do {
+            try recordingSession.setCategory(AVAudioSessionCategoryPlayAndRecord)
+            try recordingSession.setActive(true)
+            recordingSession.requestRecordPermission() { [unowned self] allowed in
+                DispatchQueue.main.async {
+                    if allowed {
+                        print("Allow")
+                    } else {
+                        print("Dont Allow")
+                    }
+                }
+            }
+        } catch {
+            print("failed to record!")
+        }
+            
+            // Audio Settings
+            
+        settings = [
+            AVFormatIDKey: Int(kAudioFormatMPEG4AAC),
+            AVSampleRateKey: 12000,
+            AVNumberOfChannelsKey: 1,
+            AVEncoderAudioQualityKey: AVAudioQuality.high.rawValue
+        ]
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +66,113 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func selectFreq(_ sender: AnyObject) {
+    
+    }
 
+    @IBAction func startStopTimer1(_ sender: AnyObject) {
+        if (!timer1.isValid) {
+            timer1 = Timer.scheduledTimer(timeInterval: 0.01, target: self, selector: #selector(self.updateTime), userInfo: nil, repeats: true)
+            startTime = Date.timeIntervalSinceReferenceDate
+        } else {
+            //you need to save the time after this
+            //also leave the timer label on until a new timer is created
+            timer1.invalidate()
+ 
+        }
+    }
+    func updateTime() {
+        let currentTime = Date.timeIntervalSinceReferenceDate
+        
+        //Find the difference between current time and start time.
+        var elapsedTime: TimeInterval = currentTime - startTime
+        
+        //calculate the minutes in elapsed time.
+        let minutes = UInt8(elapsedTime / 60.0)
+        elapsedTime -= (TimeInterval(minutes) * 60)
+        
+        //calculate the seconds in elapsed time.
+        let seconds = UInt8(elapsedTime)
+        elapsedTime -= TimeInterval(seconds)
+        
+        //find out the fraction of milliseconds to be displayed.
+        let fraction = UInt8(elapsedTime * 100)
+        
+        //add the leading zero for minutes, seconds and millseconds and store them as string constants
+        
+        let strMinutes = String(format: "%02d", minutes)
+        let strSeconds = String(format: "%02d", seconds)
+        let strFraction = String(format: "%02d", fraction)
+        
+        //concatenate minuets, seconds and milliseconds as assign it to the UILabel
+        timer1Label.text = "\(strMinutes):\(strSeconds):\(strFraction)"
+    }
+
+    func directoryURL() -> NSURL? {
+        let fileManager = FileManager.default
+        let urls = fileManager.urls(for: .documentDirectory, in: .userDomainMask)
+        let documentDirectory = urls[0] as NSURL
+        let soundURL = documentDirectory.appendingPathComponent("sound.m4a")
+        print(soundURL)
+        return soundURL as NSURL?
+    }
+    func startRecording() {
+        let audioSession = AVAudioSession.sharedInstance()
+        do {
+            audioRecorder = try AVAudioRecorder(url: self.directoryURL()! as URL,
+                                                    settings: settings)
+            audioRecorder.delegate = self
+            audioRecorder.prepareToRecord()
+        } catch {
+            finishRecording(success: false)
+        }
+        do {
+            try audioSession.setActive(true)
+            audioRecorder.record()
+        } catch {
+        }
+    }
+    func finishRecording(success: Bool) {
+        audioRecorder.stop()
+        if success {
+            print(success)
+        } else {
+            audioRecorder = nil
+            print("Somthing Wrong.")
+        }
+    }
+        
+    @IBAction func doPlay(_ sender: AnyObject) {
+        if !audioRecorder.isRecording {
+            self.audioPlayer = try! AVAudioPlayer(contentsOf: audioRecorder.url)
+            self.audioPlayer.prepareToPlay()
+            self.audioPlayer.delegate = self
+            self.audioPlayer.play()
+        }
+    }
+    @IBAction func click4Rec(_ sender: AnyObject) {
+        if audioRecorder == nil {
+            self.btnAudioRecord.setTitle("Stop", for: UIControlState.normal)
+            //self.btnAudioRecord.backgroundColor = UIColor(red: 119.0/255.0, green: 119.0/255.0, blue: 119.0/255.0, alpha: 1.0)
+            self.startRecording()
+        } else {
+            self.btnAudioRecord.setTitle("Record", for: UIControlState.normal)
+            self.btnAudioRecord.backgroundColor = UIColor(red: 221.0/255.0, green: 27.0/255.0, blue: 50.0/255.0, alpha: 1.0)
+            self.finishRecording(success: true)
+        }
+
+    }
+    func audioPlayerDidFinishPlaying(_ player: AVAudioPlayer, successfully flag: Bool) {
+        print(flag)
+    }
+    func audioPlayerDecodeErrorDidOccur(_ player: AVAudioPlayer, error: Error?){
+        print(error.debugDescription)
+    }
+    internal func audioPlayerBeginInterruption(_ player: AVAudioPlayer){
+        print(player.debugDescription)
+    }
+
+        
 }
+
 
